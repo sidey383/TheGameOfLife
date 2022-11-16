79f0cba643ca870337d5b80cc1e161d7a7b6d356
sidey383 <sidey383@mail.ru>
2022-11-17 01:19:38 +0700

    fix CMakeLists.txt, fix buildOutString in Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -7,23 +7,23 @@ Logger::Logger(std::string name): name(name) {}
 
 static std::string buildOutString(std::string format, std::string name, std::tm* time, std::string message) {
     std::string result = std::regex_replace(format, std::regex("%name"), name);
+    result = std::regex_replace(result, std::regex("%message"), message);
     result = std::regex_replace(result, std::regex("%h"), std::to_string(time->tm_hour));
     result = std::regex_replace(result, std::regex("%m"), std::to_string(time->tm_min));
     result = std::regex_replace(result, std::regex("%s"), std::to_string(time->tm_sec));
-    result = std::regex_replace(result, std::regex("%message"), message);
     return result;
 }
 
 void Logger::info(std::string message) {
     std::time_t t = std::time(0);
     std::tm* now = std::localtime(&t);
-    std::cout << buildOutString(format, name, now, message);
+    std::cout << buildOutString(format, name, now, message) << std::endl;
 }
 
 void Logger::error(std::string message) {
     std::time_t t = std::time(0);
     std::tm* now = std::localtime(&t);
-    std::cerr << buildOutString(format, name, now, message);
+    std::cerr << buildOutString(format, name, now, message) << std::endl;
 }
 
 
